b7a1ca4af3f1a39bcff7caffa17bf0b13edec40c
Marce Romagnoli <marce.romagnoli@gmail.com>
2014-06-22 20:21:54 -0300

    Allow retrieving info from process and kill by pid.

--- app/api_0_1/process.py
+++ app/api_0_1/process.py
@@ -5,19 +5,46 @@ import psutil
 @api.route('/process/')
 def show_process():
 	response = []
-	for pid in psutil.pids():
-		p = psutil.Process(pid)
-		response.append({
-			'pid': pid,
-			'name': p.name()
-		})
+	try:
+		for pid in psutil.pids():
+			response.append(_makeProcessResponse(pid))
+		return jsonify({
+			'_status': 'ok',
+			'_count': len(response),
+			'_data': response
+		}), 200
+	except Exception, e:
+		return jsonify({
+			'_status': 'fail',
+			'_reason': 'Cannot retrieve info from processes.',
+		}), 404
 
-	return jsonify({'pids': response})
 
-@api.route('/process/kill/<int:pid>')
+@api.route('/process/kill/<int:pid>', methods=['GET', 'DELETE'])
 def kill_process(pid):
+	try:
+		p = psutil.Process(pid)
+		p.kill()
+		return jsonify({
+			'_status': 'ok'
+		}), 200
+
+	except Exception, e:
+		return jsonify({
+			'_status': 'fail',
+			'_reason': str(e)
+		}), 404
+
+def _makeProcessResponse(pid):
 	p = psutil.Process(pid)
-	p.kill()
-	return jsonify({
-		'status': 'ok'
-	}), 200
+	return {
+		'pid': pid,
+		'name': p.name(),
+		'user': p.username(),
+		'cpu_in_percent': p.cpu_percent(),
+		'mem_in_percent': p.memory_percent(),
+		'vsz': p.memory_info()[1],
+		'rss': p.memory_info()[0],
+		'tty': p.terminal(),
+		'status': p.status()
+	}
